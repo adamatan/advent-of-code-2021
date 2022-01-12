7da962b3f02b649f2e33a3b74ee2faa97f1dcb8a
Adam Matan <adam@matan.name>
2022-01-12 08:47:12 +0200

    Day 2: Use typings and enums

--- 02.py
+++ 02.py
@@ -8,6 +8,25 @@ Repo and README: https://github.com/adamatan/advent-of-code-2021
 Copyright (c) 2021 Adam Matan and distributed under the MIT License.
 """
 
+from typing import NamedTuple
+
+class Line(NamedTuple):
+    """A line is a (direction, distance) tuple."""
+    direction: str
+    distance: int
+
+    @staticmethod
+    def from_string(raw_line):
+        direction, distance = raw_line.split()
+        return Line(direction, int(distance))
+
+class Sub(NamedTuple):
+    """A named tuple representing a submarine position and aim for part 2."""
+
+    horizontal: int
+    depth: int
+    aim: int
+
 
 def get_input():
     """Reads the input file into a list of lines."""
@@ -16,14 +35,14 @@ def get_input():
     return lines
 
 
-def parse_command(line):
-    """Parses a single line of the input file into a (horizontal, depth) tuple,
-    signifying the submarine's movement."""
+def parse_command_part_1(line):
+    """Takes a submarine status and a command, and creates a new sub position
+    object."""
     direction, distance = line.split()
     distance = int(distance)
     match direction:
         case "up":
-            return (0, -distance)
+            return (0, distance)
         case "down":
             return (0, distance)
         case "forward":
@@ -31,17 +50,35 @@ def parse_command(line):
         case _:
             raise ValueError(f"Unknown command: {direction}")
 
+def parse_command_part_2(line: Line, sub: Sub):
+    """Parses a single line of the input file into a (horizontal, depth) tuple,
+    signifying the submarine's movement."""
+    match line.direction:
+        case "up":
+            return Sub(sub.horizontal, sub.depth, sub.aim - line.distance)
+        case "down":
+            return Sub(sub.horizontal, sub.depth, sub.aim + line.distance)
+        case "forward":
+            return Sub(sub.horizontal + line.distance, sub.depth + line.distance * sub.aim, sub.aim)
 
-def part_1(input_lines):
+
+def part_1(input_lines) -> int:
     """Calculates the total submarine displacement as a a (horizontal, depth),
     and returns the multiplication of both axis requested by the puzzle."""
     location = (0, 0)
     for line in input_lines:
-        command = parse_command(line)
+        command = parse_command_part_1(line)
         location = (location[0] + command[0], location[1] + command[1])
     return location[0] * location[1]
 
+def part_2(input_lines) -> int:
+    sub = Sub(0, 0, 0)
+    for raw_line in input_lines:
+        line = Line.from_string(raw_line)
+        sub = parse_command_part_2(line, sub)
+    return sub.horizontal * sub.depth
 
 if __name__ == "__main__":
     input_lines = get_input()
     print(part_1(input_lines))
+    print(part_2(input_lines))
