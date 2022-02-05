ec522266db743ff91aa90b1c736363316b89295b
Adam Matan <adam@matan.name>
2022-02-05 23:59:17 +0200

    Day 7 part 1

---
+++ 07.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""
+Advent of Code sulution for day 7.
+
+Repo and README: https://github.com/adamatan/advent-of-code-2021
+
+Copyright (c) 2021 Adam Matan and distributed under the MIT License.
+"""
+
+import sys
+from collections import Counter
+from loguru import logger
+from typing import List
+
+
+def get_input(input_filename: str) -> List[int]:
+    """Same as day 6 - let's move to a shared function."""
+    with open(input_filename) as f:
+        return [int(x) for x in f.read().split(",")]
+
+
+def part_1(input_filename: str) -> int:
+    """Solves part 1 of the day 6 puzzle.
+
+    Args:
+        input_filename: The input file to read.
+
+    Returns:
+        The cheapest fuel cost of the crabs.
+
+    >>> part_1("input/07-small.txt")
+    37
+
+    A less readable version is:
+        costs = {
+        i: sum([abs(i - x) for x in crab_positions])
+        for i in range(min(crab_positions), max(crab_positions) + 1)
+    }
+    return min(costs.values())
+    """
+    crab_positions = get_input(input_filename)
+    minimal_cost: int = sys.maxsize
+    for i in range(min(crab_positions), max(crab_positions) + 1):
+        cost = sum([abs(i - x) for x in crab_positions])
+        if cost < minimal_cost:
+            minimal_cost = cost
+    return minimal_cost
+
+
+if __name__ == "__main__":
+    print(part_1("input/07.txt"))
