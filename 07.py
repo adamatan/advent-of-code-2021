d3fdcbe0864580a936e7e50e0a6674666c068fab
Adam Matan <adam@matan.name>
2022-02-06 00:16:04 +0200

    Day 7 part 2

--- 07.py
+++ 07.py
@@ -11,13 +11,36 @@ Copyright (c) 2021 Adam Matan and distributed under the MIT License.
 import sys
 from collections import Counter
 from loguru import logger
-from typing import List
+from typing import List, Callable
 
 
 def get_input(input_filename: str) -> List[int]:
     """Same as day 6 - let's move to a shared function."""
     with open(input_filename) as f:
         return [int(x) for x in f.read().split(",")]
+
+
+def gauss_sum(a, b: int):
+    """Returns the sum of the Gauss numbers up to a given distance.
+    >>> gauss_sum(1, 0)
+    1
+    >>> gauss_sum(11, 22)
+    66
+    """
+    distance = abs(a - b)
+    return distance * (distance + 1) // 2
+
+
+def calculate_minimal_cost(
+    crab_positions: List[int], distance_function: Callable[[int, int], int]
+) -> int:
+    """Calculates the minimal cost of a set of crabs, given a distance function."""
+    minimal_cost: int = sys.maxsize
+    for i in range(min(crab_positions), max(crab_positions) + 1):
+        cost = sum([distance_function(i, x) for x in crab_positions])
+        if cost < minimal_cost:
+            minimal_cost = cost
+    return minimal_cost
 
 
 def part_1(input_filename: str) -> int:
@@ -40,13 +63,25 @@ def part_1(input_filename: str) -> int:
     return min(costs.values())
     """
     crab_positions = get_input(input_filename)
-    minimal_cost: int = sys.maxsize
-    for i in range(min(crab_positions), max(crab_positions) + 1):
-        cost = sum([abs(i - x) for x in crab_positions])
-        if cost < minimal_cost:
-            minimal_cost = cost
-    return minimal_cost
+    return calculate_minimal_cost(crab_positions, lambda a, b: abs(a - b))
+
+
+def part_2(input_filename: str) -> int:
+    """Solves part 2 of the day 6 puzzle.
+
+    Args:
+        input_filename: The input file to read.
+
+    Returns:
+        The cheapest fuel cost of the crabs.
+
+    >> part_2("input/07.txt")
+    99540554
+    """
+    crab_positions = get_input(input_filename)
+    return calculate_minimal_cost(crab_positions, gauss_sum)
 
 
 if __name__ == "__main__":
     print(part_1("input/07.txt"))
+    print(part_2("input/07.txt"))
